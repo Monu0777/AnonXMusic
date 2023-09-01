5b758d6f43abbbb68b4618eeed63189a8a558161
Monu0777 <143521089+Monu0777@users.noreply.github.com>
2023-09-01 15:54:12 +0530

    Update config.py
    Signed-off-by: Monu0777 <143521089+Monu0777@users.noreply.github.com>

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝄟⃝⃝𝗗𝗲𝘃𝗶𝗹_𝗠𝘂𝘀𝗶𝗰᭄࿐")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6467722524").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1651859190").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
