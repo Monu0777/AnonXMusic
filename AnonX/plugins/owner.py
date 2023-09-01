e4353908f2d8fd22623005e2635d9547ccc12e98
Monu0777 <143521089+Monu0777@users.noreply.github.com>
2023-09-01 15:20:27 +0530

    Update owner.py
    Signed-off-by: Monu0777 <143521089+Monu0777@users.noreply.github.com>

--- AnonX/plugins/owner.py
+++ AnonX/plugins/owner.py
@@ -58,7 +58,7 @@ async def help(client: Client, message: Message):
     & ~filters.edited & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph//file/a3d20f525637eed3cc092.jpg",
+        photo=f"https://telegra.ph/file/0aad28613ca8630da10ee.jpg",
         caption=f"""🍁𝐂ʅυƈ𝐊🥰𝐁ҽʅσ𝐖💝𝐁υƚƚσ𝐍✨𝐓σ🙊𝐆ҽ𝐓🌱𝐒υρρσɾ𝐓🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -76,7 +76,7 @@ async def help(client: Client, message: Message):
     & ~filters.edited & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph//file/a3d20f525637eed3cc092.jpg",
+        photo=f"https://telegra.ph/file/0aad28613ca8630da10ee.jpg",
         caption=f"""🍁𝐂ʅιƈ𝐊🥰𝐁ҽʅσ𝐖💝𝐁υƚƚσ𝐍✨𝐓σ🙊𝐆ҽ𝐓🌱𝐒υρρσ𝐓🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
