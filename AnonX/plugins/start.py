9e76743a684678fe9e3735c878ea083431c67ce5
Monu0777 <143521089+Monu0777@users.noreply.github.com>
2023-09-01 17:53:53 +0530

    Update start.py
    Signed-off-by: Monu0777 <143521089+Monu0777@users.noreply.github.com>

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("https://telegra.ph/file/cb02eadfceb1e13e38da0.png")
+            await message.reply_sticker("CAACAgUAAx0CbnBtaAABGUZrZPHW0HcG8yGDI8V1Noq2Akg_0NcAAvMJAAJ7fSBWswABx2I7cSlDHgQ")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("https://telegra.ph/file/cb02eadfceb1e13e38da0.png")
+                await message.reply_sticker("CAACAgUAAx0CbnBtaAABGUZrZPHW0HcG8yGDI8V1Noq2Akg_0NcAAvMJAAJ7fSBWswABx2I7cSlDHgQ")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
